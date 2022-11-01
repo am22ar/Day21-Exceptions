5d2631cf706f2c1b1837edbae4828d4b4320acd1
am22ar <amarpatil12222@gmail.com>
2022-11-01 14:49:58 +0530

    [Added: happy/sad mood informed to user]

--- src/com/java/exceptions/MoodAnalyserException.java
+++ src/com/java/exceptions/MoodAnalyserException.java
@@ -1,8 +1,14 @@
 package com.java.exceptions;
 
-public class MoodAnalyserException extends Exception{
-	 public MoodAnalyserException(String message)
-	    {
-	        super(message);
-	    }
+public class MoodAnalyserException extends Exception {
+	public enum ExceptionType {
+		ENTERED_NULL, ENTERED_EMPTY;
+	}
+
+	ExceptionType type;
+
+	public MoodAnalyserException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
 }
